88d3898643147aeedd2c2d86f81fa1b0a935f66c
satoshiwasHere <skinyanjui100@gmail.com>
2023-08-08 21:38:04 +0000

    Advanced Api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and
-returns a list containing the titles of all hot articles for a subreddit.
+Queries the Reddit API recrusively then
+returns a list containing the titles of all hot articles for a subreddit
 """
 import requests
 after = None
 
 
 def recurse(subreddit, hot_list=[]):
-   """returning hot top ten post titles recursively"""
+    """
+    returning hot top ten post titles recursively
+    """
 
     global after
     user_agent = {'User-Agent': 'api_advanced-project'}
